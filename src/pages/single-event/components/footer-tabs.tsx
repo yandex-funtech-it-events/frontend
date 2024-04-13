82f08b645b16211ac78201f2bc2c9ac6dc3f088c
Anastasia <www.nastya97@yandex.ru>
2024-04-13 20:32:53 +0300

    feat: all sections

--- src/pages/single-event/components/footer-tabs.tsx
+++ src/pages/single-event/components/footer-tabs.tsx
@@ -1,31 +1,43 @@
 import React from 'react';
-import { Box, Link } from '@mui/material';
+import { Box, Link, Container } from '@mui/material';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { footerNavArray } from '../../../libs/constants';
 
 const FooterTabs: React.FC = () => {
   return (
-    <Box
-      component="section"
-      display="flex"
-      justifyContent="center"
-      mb={5}
-      sx={{ width: '100%', border: '1px solid red' }}
-    >
-      <nav style={{ gap: '16px', display: 'flex', alignItems: 'center' }}>
-        <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>О мероприятии</Link>
-        <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>Спикеры</Link>
-        <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>Программа</Link>
-        <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>Регистрация</Link>
-        <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>Карта</Link>
-        <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>Сообщества</Link>
-        <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>Вопросы</Link>
-        <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>Прошлый раз</Link>
-        <Box display="flex" gap={1}>
-          <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>В начало</Link>
-          <ExpandLessIcon />
+    <Container>
+      <Box
+        component="section"
+        display="flex"
+        justifyContent="center"
+        mb={10}
+        sx={{ width: '100%', border: '1px solid red' }}
+      >
+        <Box
+          borderRadius={1}
+          sx={{
+            border: '1px solid green',
+            padding: '6px 16px',
+            background: '#E0E0E0',
+          }}
+        >
+          <nav style={{ gap: '32px', display: 'flex', alignItems: 'center' }}>
+            {footerNavArray.map((item, i) =>
+              i === footerNavArray.length - 1 ? (
+                <Box key={i} display="flex" gap={1}>
+                  <Link sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>В начало</Link>
+                  <ExpandLessIcon />
+                </Box>
+              ) : (
+                <Link key={i} sx={{ textDecoration: 'none', textTransform: 'uppercase' }}>
+                  {item.name}
+                </Link>
+              )
+            )}
+          </nav>
         </Box>
-      </nav>
-    </Box>
+      </Box>
+    </Container>
   );
 };
 
